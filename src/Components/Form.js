582b8b047d449fbeaa5a9537c411e032ca673b2c
Dev Govindji <friendsofdev94@gmail.com>
2019-12-03 15:50:17 -0600

    stubs flow of form component

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,14 +3,51 @@ import ReactDOM from 'react-dom';
 
 
 class Form extends React.Component {
+    state = {
+        name: '',
+        email: '',
+        phone: '',
+        subject: '',
+        comment: ''
 
-    // handlChange(typing)
+    }
+
+    // handleOnChange(typing)
     // handing submit
+
+    handleOnChange() {
+
+    }
+
+    onSubmit() {
+
+    }
 
     render() {
         return (
-            <form>
-                form renders
+            <form onSubmit={this.onSubmit}>
+                <h1>Form</h1>
+                <label>Name</label>
+                <input
+                    type='text'
+                    value='name'
+                    onChange={this.handleOnChange}
+                    value={this.name} />
+
+                <label>Email</label>
+                <input />
+
+                <label>Phone</label>
+                <input />
+
+                <label>Subject</label>
+                <input />
+
+                <label>Comment</label>
+                <input />
+
+                <input type='button' value='Submit' />
+
             </form >
         )
     }
