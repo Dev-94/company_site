22b6907194d6e04a5bb60565956a21e1abcdc141
Dev Govindji <friendsofdev94@gmail.com>
2019-12-03 16:45:52 -0600

    adds nave links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Link, BrowserRoute as Router } from 'react-router-dom'
+// import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Nav, Router, Route, Link } from 'react-router-dom'
+
+// import Container from 'react-bootstrap/Container'
+// import Button from 'react-bootstrap/Button'
+// import NavbarBrand from 'react-bootstrap/NavbarBrand'
+// import NavBar from 'react-bootstrap/Navbar'
+// import Nav from 'react-bootstrap/Nav'
 import Title from './Components/Title'
 import Form from './Components/Form'
 
@@ -12,6 +19,17 @@ class App extends React.Component {
 
     return (
       <div className="App">
+        {/* <Router> */}
+        <Nav>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contact</Link>
+        </Nav>
+        {/* </Router> */}
+
+
+
+
+
         <Title />
 
         <Form />
