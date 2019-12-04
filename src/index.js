4c76bee97756303cb2e152584f074bba748ad944
Dev Govindji <friendsofdev94@gmail.com>
2019-12-04 14:39:08 -0600

    installs react google recaptcha library

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 
 import Title from './Components/Title'
 import Form from './Components/Form'
@@ -7,7 +8,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<GoogleReCaptchaProvider>
+    <App />
+</GoogleReCaptchaProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
